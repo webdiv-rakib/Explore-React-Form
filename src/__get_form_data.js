bed1d0729cdb77be7ca10aad29122fa635b6fa1b
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-03-12 05:07:19 +0600

    Access form data using onSubmit and e target

---
+++ src/__get_form_data.js
@@ -0,0 +1,4 @@
+/**
+ * To get input data
+ * 1. e.target.[name of the input field].value
+ */
--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 const SimpleForm = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(e.target.name.value)
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
     }
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <input type="text" name="name" id="" placeholder='Enter Your Name' />
+                <br />
+                <input type="email" name="email" id="" placeholder='Enter Your Email' />
                 <br />
                 <button type="submit" value="Submit">Submit</button>
             </form>
