f2c63b72754689dbe5bfced2b8017559da560229
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-03-12 05:13:27 +0600

    added another form.

---
+++ src/components/FormAction/FormAction.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FormAction = () => {
+    return (
+        <div>
+            <form action="">
+
+            </form>
+        </div>
+    );
+};
+
+export default FormAction;
